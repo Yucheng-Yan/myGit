808e960b8d26a5b4005de095f2736aa4a44ef1ad
Coder <lcthw@Yuchengs-MacBook-Air.local>
2024-01-24 12:01:13 +1100

    init

--- mygit.py
+++ mygit.py
@@ -1,12 +1,27 @@
-import argparse
+import argparse, os
 
-if __name__ == "main":  
+def init(repo):
+    """Create directory repo and .git directory."""
+    os.mkdir(repo)
+
+
+if __name__ == "__main__":  
     # Create a new ArgumentParser object.
     parser = argparse.ArgumentParser()       
 
-    # dest:     Name of the attribute under which sub-command name will be stored.
-    # metavar:  String presenting available sub-commands in help.
+    # dest:     Name of the attribute under which sub-command name will be
+    # stored.  metavar:  String presenting available sub-commands in help.
     # required: Set True to force the option.
-    sub_parsers = parser.add_subparsers(dest='command', metavar='command', required=True)     
+    sub_parsers = parser.add_subparsers(dest='command', metavar='command',
+                                        required=False)     
+    
+    # when get the user input init, call this specific sub_parser
+    sub_parser = sub_parsers.add_parser('init',
+                help='initialize a new repo')
+    sub_parser.add_argument('repo',
+                help='directory name for new repo')
+    
 
-
+    args = parser.parse_args()
+    if args.command == "init":
+        init(args.repo)
